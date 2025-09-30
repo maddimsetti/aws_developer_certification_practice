d8721f2b1b7d7940bb23f1e083367c9488c87e08
BALA RAMA KRISHNA MADDIMSETTI <krishnabalaram@BALAs-MacBook-Air.local>
2025-09-30 05:47:29 +0530

    Added the code how the sqs triggers to the consumer lambda

--- src/main/java/com/balarama/awslearing/lambda_leading/controller/SqsController.java
+++ src/main/java/com/balarama/awslearing/lambda_leading/controller/SqsController.java
@@ -1,5 +1,7 @@
 package com.balarama.awslearing.lambda_leading.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -42,22 +44,22 @@ public class SqsController {
 	@GetMapping("/receivemessages")
 	public ResponseEntity<APIResponse> receiveMessages(RequestEntity<Void> request) {
         try {
-            sqsService.receiveMessage();
-            return ResponseEntity.status(HttpStatus.OK).body(new APIResponse("success", "📥 Checked for messages"));
+            List<String> message = sqsService.receiveMessagefromSQS();
+            return ResponseEntity.status(HttpStatus.OK).body(new APIResponse("success", message, "📥 Checked for messages"));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new APIResponse("error", e.getMessage()));
+                    .body(new APIResponse("error", null, e.getMessage()));
         }
     }
 	
 	@GetMapping("/dlq/receive")
     public ResponseEntity<APIResponse> receiveDlqMessages(RequestEntity<Void> request) {
         try {
-            sqsService.receiveDlqMessages();
-            return ResponseEntity.status(HttpStatus.OK).body(new APIResponse("success", "📥 Checked for DLQ messages"));
+            List<String> message = sqsService.receiveDlqMessages();
+            return ResponseEntity.status(HttpStatus.OK).body(new APIResponse("success", message, "📥 Checked for DLQ messages"));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new APIResponse("error", e.getMessage()));
+                    .body(new APIResponse("error", null, e.getMessage()));
         }
     }
 }
